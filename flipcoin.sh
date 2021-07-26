bf7431b6a443387b8d2b86c94957f86389c1af04
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 15:04:19 +0530

    multiple flip

--- flipcoin.sh
+++ flipcoin.sh
@@ -6,3 +6,17 @@ if [ $flip -eq 0 ] ; then
 else
 	echo Tails
 fi
+
+read -p "Enter number of times you want to flip the coin : " flip_number
+
+while [ $flip_number -ne 0 ] ; do
+	((flip=RANDOM%2))
+	if [ $flip -eq 0 ] ; then
+		((heads_count+=1))
+	else
+		((tails_count+=1))
+	fi
+	((flip_number-=1))
+done
+
+echo Heads Score : $heads_count --- Tails Score : $tails_count
