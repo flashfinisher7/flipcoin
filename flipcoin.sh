5db425d0e2bdde46267fe9dba72b0cd5e2bbfc5d
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-26 15:09:27 +0530

    output UC3,4

--- flipcoin.sh
+++ flipcoin.sh
@@ -20,3 +20,24 @@ while [ $flip_number -ne 0 ] ; do
 done
 
 echo Heads Score : $heads_count --- Tails Score : $tails_count
+
+heads_count=0
+tails_count=0
+while [ $heads_count -ne 21 -a $tails_count -ne 21 ] ; do
+	((flip=RANDOM%2))
+	if [ $flip -eq 0 ] ; then
+		((heads_count+=1))
+	else
+		((tails_count+=1))
+	fi
+done
+
+if [ $heads_count -eq 21 ] ; then
+	echo Heads WON!
+	echo Head Won by $(($heads_count-$tails_count))
+elif [ $tails_count -eq 21 ] ; then
+	echo Tails WON!
+        echo Tails Won by $(($tails_count-$heads_count))
+fi
+
+echo Heads Score : $heads_count --- Tails Score : $tails_count
